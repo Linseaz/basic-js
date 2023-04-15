bb1e44e576aaad6d424435f5b295b61ef97f82ef
linseaz <linsea@online.ua>
2023-04-15 13:25:18 +0300

    feat: common-character finish

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,7 +12,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(s1, s2) {
+  const arr1 = s1.split('');
+  const arr2 = s2.split('');
+  let counter = 0;
 
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr1.includes(arr2[i])) {
+      counter++;
+
+      const j = arr1.indexOf(arr2[i]);
+      arr1.splice(j, 1);
+    }
+  }
+  return counter;
 }
 
 module.exports = {
