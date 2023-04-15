8fbeb43114491dd7be45fd2852bd2ef82ec3a2c6
linseaz <linsea@online.ua>
2023-04-15 14:47:52 +0300

    feat: dns-stats finish

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -7,11 +7,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * @return {Object}
  *
  * @example
- * domains = [
- *  'code.yandex.ru',
- *  'music.yandex.ru',
- *  'yandex.ru'
- * ]
+  
  *
  * The result should be the following:
  * {
@@ -22,9 +18,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function getDNSStats(domains) {
+  const result = {};
+
+  for (const domain of domains) {
+    const segments = domain
+      .split('.')
+      .reverse();
+
+    let current = '';
+    console.log(current)
+    for (const segment of segments) {
+      current += `.${segment}`;
+      result[current] = (result[current])
+        ? result[current] + 1
+        : 1
+    }
+  }
+  return result;
 }
 
 module.exports = {
